9b4e2d8e12a5161190497a3f01c69930cf707832
Julian Heinen <heinen@weiss-intermedia.de>
2024-01-11 16:28:03 +0100

    Initiate tailwind and fontawesome

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./flaskr/templates/**/*.html",
+    "./flaskr/static/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
